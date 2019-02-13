7a43d164226d1668012e2ebfaf2281476aadc1b0
Sydney Dunlap <sydney.dunlap2011@gmail.com>
2019-02-13 12:43:37 -0500

    fixed formatting and changed a few comments around in js

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-
     // Variable holding questions
-    var questionArray = ["Sailing is an Olympic sport. What year was it first included in the Olympics?",
+    let questionArray = ["Sailing is an Olympic sport. What year was it first included in the Olympics?",
         "On a sailboat, what is a sheet?",
         "When someone asks you to grab the tiller, what are they asking you to do?",
         "The right side of a boat is referred to as what?",
@@ -9,19 +8,21 @@ $(document).ready(function () {
         "What are halyards used for?",
         "Which of the following is NOT a turning maneuver in sailing?",
         "What color flag must be flown when first entering the territorial waters of another country?",
-        "Large buoys used to denote a safe channel or passage for vessels in and out of the harbor are what color?"];
+        "Large buoys used to denote a safe channel or passage for vessels in and out of the harbor are what color?"
+    ];
     // Variable holding answers
-    var answerArray = [["1896", "1904", "1920", "1944"],
-    ["Another name for the sail", "A thin blanket on the bed", "The chartbook you're referencing", "Lines used for controlling and adjusting sails"],
-    ["Tend to any plants and/or herb garden nearby", "Steer the boat", "Get the anchor ready", "Check coordinates and plot location on charts"],
-    ["Bow", "Starboard", "Stern", "Port"],
-    ["Ketch", "Schooner", "Yawl", "Sloop"],
-    ["To hail nearby boats during an emergency", "A tool used for measuring distance", "Used to raise and lower the sails", "Controls the movement of the sails"],
-    ["Jibing", "Tacking", "Sail trimming", "Hull trimming"],
-    ["White", "Red", "Yellow", "Green"],
-    ["Blue & White", "Black & White", "Red & Yellow", "Red & Green"]];
+    let answerArray = [["1896", "1904", "1920", "1944"],
+        ["Another name for the sail", "A thin blanket on the bed", "The chartbook you're referencing", "Lines used for controlling and adjusting sails"],
+        ["Tend to any plants and/or herb garden nearby", "Steer the boat", "Get the anchor ready", "Check coordinates and plot location on charts"],
+        ["Bow", "Starboard", "Stern", "Port"],
+        ["Ketch", "Schooner", "Yawl", "Sloop"],
+        ["To hail nearby boats during an emergency", "A tool used for measuring distance", "Used to raise and lower the sails", "Controls the movement of the sails"],
+        ["Jibing", "Tacking", "Sail trimming", "Hull trimming"],
+        ["White", "Red", "Yellow", "Green"],
+        ["Blue & White", "Black & White", "Red & Yellow", "Red & Green"]
+    ];
     // Variable holding correct answers
-    var correctAnswerArray = ["1896",
+    let correctAnswerArray = ["1896",
         "Lines used for controlling and adjusting sails",
         "Steer the boat",
         "Starboard",
@@ -29,54 +30,51 @@ $(document).ready(function () {
         "Used to raise and lower the sails",
         "Hull trimming",
         "Yellow",
-        "Red & Green"];
-    // Variables to hold message screen after user guesses or time is up:
-    var userCorrect = "Correct!!!";
-    var userIncorrect = "Incorrect!";
-    var timerOut = "You're out of time!!!"
-
-    // Variable to hold the explanation when user selects the wrong answer or runs out of time:
-    var answerExplain = ["Sailing was introduced into the Olympics at the Games held in Athens, Greece in 1896. Although sailing was on the program for that year it could not take place because of weather concerns. Sailing was not included in the 1900 Olympics, but returned to the regular program in 1904.",
-    "On most sailboats there are three sheets-the port jib sheet, the starboard jib sheet and the mainsheet. The jib sheets are the lines (ropes) that control the jib sail and the main sheet controls the mainsail. The saying 'being three sheets to the wind' referring to someone who is extremely drunk or out of control comes from this sailing term. On a sailboat if all three of your sheets are out flapping in the wind you have no control over your sails and thus your sailboat.",
-    "A tiller is a straight piece of wood or metal which fits into the head of the rudder and is used for steering a boat.",
-    "From the rear of the boat looking forward, the left side of the boat is port and the right side is starboard.",
-    "The schooner has a fore mast which is shorter than the main mast. The ketch and the yawl both have a mizzen mast, aft of the main mast, but the mizzen mast on a yawl is located aft of the steering post. The easy way to remember which is which is that 'k' comes before 'y.' The sloop has only one mast.",
-    "Halyards are used to raise and lower the sails!",
-    "Jibing is turning the stern past the wind so the wind changes from one side to the other, allowing a turn. Tacking is changing the bow into the wind using the wind change from side to side to help the turn. Sail trimming is simply setting the angle of the vessel to the wind. Hull trimming however is adjusting the boat's load to work with its performance in the water.",
-    "The yellow flag is the international signal flag for, 'my vessel is healthy.' It is known as the 'Q' flag, and in effect invites Customs Officers to inspect your vessel. Once inspected, or after a specified period awaiting inspection, the Q flag can be lowered, and need not be raised again while inside the territorial waters of that country.",
-    "When entering a harbour, these 'lateral marks' as they are known, must be kept green to starboard and red to port. On leaving harbour, the green buoys must be kept to port and the red to starboard. When navigating a channel, either leaving or entering a harbour, you must keep your vessel to your right hand side of the channel. Buoys that warn of danger, and many others, are standardised across the world. However, in the system in use in the United States, the lateral mark system is reversed; on leaving a harbour the green lateral buoys must be kept to starboard, the red to port and vice versa on entering a harbour. The opening of a safe channel may also be indicated by a pillar buoy painted with alternating red/white vertical stripes."];
-
-    // An array to hold the gifs to show after user is correct or incorrect:
-    var giphyArray = ["<img class='giphy' src='https://media.giphy.com/media/jGC37Ky8b5OxO/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/3o7aCVTfelG4XSbv3y/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/fHua9RHmk5I4Ry8X7S/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/3o6MbbDfnFzOS0SYes/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/3oriNLze0eyurnHTwI/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/xUPGcFdM5qI3G05Ng4/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/5Mnb0r8Qdvv6U/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/PNfPqeuSXnFTYKYn9P/giphy.gif'>",
-    "<img class='giphy' src='https://media.giphy.com/media/l0MYSryDg63YEmXVS/giphy.gif'>"
+        "Red & Green"
     ];
-
+    // Variables to hold message screen after user guesses or time is up:
+    let userCorrect = "Correct!!!";
+    let userIncorrect = "Incorrect!";
+    let timerOut = "You're out of time!!!";
+    // Variable to hold the explanation when user selects the wrong answer or runs out of time:
+    let answerExplain = ["Sailing was introduced into the Olympics at the Games held in Athens, Greece in 1896. Although sailing was on the program for that year it could not take place because of weather concerns. Sailing was not included in the 1900 Olympics, but returned to the regular program in 1904.",
+        "On most sailboats there are three sheets-the port jib sheet, the starboard jib sheet and the mainsheet. The jib sheets are the lines (ropes) that control the jib sail and the main sheet controls the mainsail. The saying 'being three sheets to the wind' referring to someone who is extremely drunk or out of control comes from this sailing term. On a sailboat if all three of your sheets are out flapping in the wind you have no control over your sails and thus your sailboat.",
+        "A tiller is a straight piece of wood or metal which fits into the head of the rudder and is used for steering a boat.",
+        "From the rear of the boat looking forward, the left side of the boat is port and the right side is starboard.",
+        "The schooner has a fore mast which is shorter than the main mast. The ketch and the yawl both have a mizzen mast, aft of the main mast, but the mizzen mast on a yawl is located aft of the steering post. The easy way to remember which is which is that 'k' comes before 'y.' The sloop has only one mast.",
+        "Halyards are used to raise and lower the sails!",
+        "Jibing is turning the stern past the wind so the wind changes from one side to the other, allowing a turn. Tacking is changing the bow into the wind using the wind change from side to side to help the turn. Sail trimming is simply setting the angle of the vessel to the wind. Hull trimming however is adjusting the boat's load to work with its performance in the water.",
+        "The yellow flag is the international signal flag for, 'my vessel is healthy.' It is known as the 'Q' flag, and in effect invites Customs Officers to inspect your vessel. Once inspected, or after a specified period awaiting inspection, the Q flag can be lowered, and need not be raised again while inside the territorial waters of that country.",
+        "When entering a harbour, these 'lateral marks' as they are known, must be kept green to starboard and red to port. On leaving harbour, the green buoys must be kept to port and the red to starboard. When navigating a channel, either leaving or entering a harbour, you must keep your vessel to your right hand side of the channel. Buoys that warn of danger, and many others, are standardised across the world. However, in the system in use in the United States, the lateral mark system is reversed; on leaving a harbour the green lateral buoys must be kept to starboard, the red to port and vice versa on entering a harbour. The opening of a safe channel may also be indicated by a pillar buoy painted with alternating red/white vertical stripes."
+    ];
+    // An array to hold the gifs to show after user is correct or incorrect:
+    let giphyArray = ["<img class='giphy' src='https://media.giphy.com/media/jGC37Ky8b5OxO/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/3o7aCVTfelG4XSbv3y/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/fHua9RHmk5I4Ry8X7S/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/3o6MbbDfnFzOS0SYes/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/3oriNLze0eyurnHTwI/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/xUPGcFdM5qI3G05Ng4/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/5Mnb0r8Qdvv6U/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/PNfPqeuSXnFTYKYn9P/giphy.gif'>",
+        "<img class='giphy' src='https://media.giphy.com/media/l0MYSryDg63YEmXVS/giphy.gif'>"
+    ];
     // An array to hold the gifs to show after a user runs out of time:
-    var timeUpGif = "<img class='giphy' src='https://media.giphy.com/media/DcshtTtOSpPO/giphy.gif'>"
-
+    let timeUpGif = "<img class='giphy' src='https://media.giphy.com/media/DcshtTtOSpPO/giphy.gif'>";
     // Variables set at the beginning of the game
-    var question = 0;
-    var countdown = 20;
-    var intervalId;
+    let question = 0;
+    let countdown = 20;
+    let intervalId;
     // Variables to hold score
     let correct = 0;
     let incorrect = 0;
+    // Print the questions to HTML
+    let gameHTML = $("#quiz");
 
     // Hide answer buttons until game is started.
     $("#quiz").hide();
 
     // Hide restart button until game is ended.
     $(".restart").hide();
-
-    // Print the questions to HTML
-    var gameHTML = $("#quiz")
 
     // Function when button is clicked
     $(".start").click(function () {
@@ -87,10 +85,8 @@ $(document).ready(function () {
         // Call the timer function
     });
 
-    // Create startGame function
     function startGame() {
         $(gameHTML).show();
-        
         countdown = 20;
         $("#timer").html("Time Left: " + countdown);
         // show question
@@ -103,45 +99,40 @@ $(document).ready(function () {
         $("#a4").html(answerArray[question][3]);
     };
 
-    // Create a timer function
     function countDown() {
         clearInterval(intervalId);
         intervalId = setInterval(decrement, 1000);
     }
-        function decrement() {
-            countdown--;
-            if (countdown === 0) {
-                clearInterval(decrement);
-                console.log("Time's Up")
-                stop();
-                timesUp();
-            }
-            $("#timer").html("Time Left: " + countdown);
+    function decrement() {
+        countdown--;
+        if (countdown === 0) {
+            clearInterval(decrement);
+            console.log("Time's Up")
+            stop();
+            timesUp();
         }
-    
+        $("#timer").html("Time Left: " + countdown);
+    }
 
     // Stop the timer from counting down
     function stop() {
         clearInterval(intervalId);
     }
 
-    // Create a function for when an answer is clicked with if then statements
+    // Logic for what happens when an answer option is clicked
     $("body").on("click", ".answer", function (event) {
         let userAnswer = $(this).text();
-
         // If for correct answer:
         if (userAnswer === correctAnswerArray[question]) {
             correctAnswer();
         }
-
         // Else for incorrect answer:
         else {
             incorrectAnswer();
         }
-
     });
 
-    // A function to run when the correct answer is selected
+    // When the correct answer is selected:
     function correctAnswer() {
         correct++;
         $(gameHTML).hide();
@@ -151,7 +142,7 @@ $(document).ready(function () {
         console.log("Correct Answers: " + correct);
     };
 
-    // A function to run when the incorrect answer is selected
+    // When the incorrect answer is selected:
     function incorrectAnswer() {
         incorrect++;
         $(gameHTML).hide();
@@ -161,7 +152,7 @@ $(document).ready(function () {
         console.log("Incorrect Answers: " + incorrect);
     }
 
-    // A function to run when the timer is up
+    // When the timer is out of time:
     function timesUp() {
         incorrect++;
         $(gameHTML).hide();
@@ -171,7 +162,7 @@ $(document).ready(function () {
         console.log("Incorrect Answers: " + incorrect);
     }
 
-    // A function to run the next question in the array 
+    // This runs the next question in the array & is called on when the response timer is out of time.
     function next() {
         if (question < 8) {
             $("#response").hide();
@@ -185,7 +176,7 @@ $(document).ready(function () {
         }
     }
 
-    // A function to show the final score at the end of the quiz
+    // Print the user's score at the end of the game:
     function finalScore() {
         $("#response").hide();
         $("#results").html("Game Over!" + "<br><br>" +
